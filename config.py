267f57968b225843c2349d6562a06681690d01c9
young-infiswift <young@infiswift.tech>
2020-04-25 19:23:23 -0700

    github

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://oranehzkghexin:9530bcc4898247cbd7385fff8f1a3f66f17ddb5e54c3f05f5fd493129cc3340a@ec2-34-197-212-240.compute-1.amazonaws.com:5432/dasqjqdnj4ff1k'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Akitohimura1@localhost:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
